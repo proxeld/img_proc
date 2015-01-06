0460a5711fa0d9d5d38a868dfc046e55647c2c43
Kuba Pelczar <jakub.pelczar@sizeof.pl>
2015-01-07 00:47:05 +0100

    Console menu added

--- Makefile
+++ Makefile
@@ -16,9 +16,13 @@ test: compile
 	mkdir out
 	@$(ERL_SHELL) -pa ebin/ -pa deps/erl_img/ebin/ -run img_proc test -run init stop -noshell
 
+testMenu: compile	
+	@$(ERL_SHELL) -pa ebin/ -pa deps/erl_img/ebin/ -run img_proc testMenu -run init stop -noshell
+
 clean: 
 	@$(REBAR) clean
 	rm -rf *.beam *.dump
 	rm -rf out/
+	mkdir out
 
 .PHONY: clean
--- src/img_proc.erl
+++ src/img_proc.erl
@@ -5,7 +5,7 @@
 -export([load/1, save/1, save/2,
 		filterGauss/1, filterAverage/1, filterMedian/1, filterMin/1, filterMax/1,
 		erode/1, dilate/1, open/1, close/1, tophat/1, bothat/1,
-		test/0, test_time/0]).
+		test/0, testMenu/0,  test_time/0]).
 
 %**************************************
 % Main module with image processing API
@@ -135,6 +135,102 @@ test() ->
 	save(Close, ?OUT_DIR ++ "close.png"),
 	save(Tophat, ?OUT_DIR ++ "tophat.png"),
 	save(Bothat, ?OUT_DIR ++ "bothat.png").
+
+testMenu() ->
+	io:format("\e[J"),
+	io:format("-------------------------------------------~n"),
+	io:format("-        IMG PROCESSING IN ERLANG         -~n"),
+	io:format("-------------------------------------------~n"),
+	io:format("-       Enter path to png image file      -~n"),
+	io:format("- (or leave blank to use Lena as default) -~n"),
+	io:format("-------------------------------------------~n"),
+	ImgPath = io:get_line("Image> "),
+	{_, ErlImg} = testImgOpen(ImgPath),
+	io:format("-------------------------------------------~n"),
+	io:format("-       Select operation to perform       -~n"),
+	io:format("-------------------------------------------~n"),
+	io:format("-                FILTERS                  -~n"),
+	io:format("-------------------------------------------~n"),
+	io:format("- 1) Gaussian                             -~n"),
+	io:format("- 2) Mean                                 -~n"),
+	io:format("- 3) Median                               -~n"),
+	io:format("-------------------------------------------~n"),
+	io:format("-           MORPHOLOGY-BASED              -~n"),
+	io:format("-------------------------------------------~n"),
+	io:format("- 4) Erosion                              -~n"),
+	io:format("- 5) Dilation                             -~n"),
+	io:format("- 6) Opening                              -~n"),
+	io:format("- 7) Closing                              -~n"),
+	io:format("- 8) Top-Hat transformation               -~n"),
+	io:format("- 9) Bottom-Hat transformation            -~n"),
+	io:format("-------------------------------------------~n"),
+	Operation = io:get_line("Number> "),
+	case Operation of
+		"1\n" -> 
+			io:format("-           Gaussian filter               -~n"),
+			Gauss = filterGauss(ErlImg),
+			save(Gauss, ?OUT_DIR ++ "gauss.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "gauss.png"]);
+
+		"2\n" -> 
+			io:format("-             Mean filter                 -~n"),
+			Avg = filterAverage(ErlImg),
+			save(Avg, ?OUT_DIR ++ "mean.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "mean.png"]);
+
+		"3\n" -> 
+			io:format("-            Median filter                -~n"),
+			Med = filterMedian(ErlImg),
+			save(Med, ?OUT_DIR ++ "median.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "median.png"]);
+
+		"4\n" -> 
+			io:format("-                Erosion                  -~n"),
+			Erode = erode(ErlImg),
+			save(Erode, ?OUT_DIR ++ "erosion.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "erosion.png"]);
+
+		"5\n" -> 
+			io:format("-                Dilation                 -~n"),
+			Dil = dilate(ErlImg),
+			save(Dil, ?OUT_DIR ++ "dilation.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "dilation.png"]);
+
+		"6\n" -> 
+			io:format("-                Opening                  -~n"),
+			Open = open(ErlImg),
+			save(Open, ?OUT_DIR ++ "opening.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "opening.png"]);
+
+		"7\n" -> 
+			io:format("-                Closing                  -~n"),
+			Close = close(ErlImg),
+			save(Close, ?OUT_DIR ++ "closing.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "closing.png"]);
+
+		"8\n" -> 
+			io:format("-        Top-Hat transformation           -~n"),
+			Tophat = tophat(ErlImg),
+			save(Tophat, ?OUT_DIR ++ "tophat.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "tophat.png"]);
+
+		"9\n" -> 
+			io:format("-      Bottom-Hat transformation          -~n"),
+			Bothat = bothat(ErlImg),
+			save(Bothat, ?OUT_DIR ++ "bothat.png"),
+			io:format("Output file saved to: ~s !~n", [?OUT_DIR ++ "bothat.png"]);
+
+		_ -> io:format("-         Wrong operation!               -~n")
+	end,
+	testMenu().
+
+testImgOpen(ImgPath) ->
+	Path = lists:sublist(ImgPath, 1, utils:len(ImgPath)-1),
+	io:format("Path: ~s~n", [Path]),
+	case Path of
+		"" -> load("priv/lena.png");
+		_  -> load(Path)
+	end.
 
 test_time() -> 
 	{_, ErlImg} = load("priv/lenaSzum256.png"),
